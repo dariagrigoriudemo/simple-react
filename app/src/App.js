fd5adde7af9957a678fa5ef542e71cda2b9c7733
dariagrigoriudemo <d.grigoriu@live.com>
2019-08-27 13:07:45 -0700

    Build demo

--- app/src/App.js
+++ app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Static App
+          Hello Static App Demo
         </p>
         <a
           className="App-link"
